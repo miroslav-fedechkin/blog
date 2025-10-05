73907104db6551900a6d596dc43b93f2fc48e77e
Miroslav <fedechkin.miroslav@yandex.ru>
2025-10-05 16:19:03 +0200

    feat(api): model for post

---
+++ app/posts/models.py
@@ -0,0 +1,11 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String
+
+class Post(Base):
+    __tablename__='posts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    
+
