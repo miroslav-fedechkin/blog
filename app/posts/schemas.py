a850dc76db7ba568828d799c61c04f282cdf0552
Miroslav <fedechkin.miroslav@yandex.ru>
2025-10-05 17:57:46 +0200

    feat(api): add response model

--- app/posts/schemas.py
+++ app/posts/schemas.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 
 class SPost(BaseModel):
+    title: str
+    content: str
+
+class SResponsePost(BaseModel):
     id: int
     title: str
     content: str
+
+    class Config:
+        from_attributes: True
