cdbf63361748bfbd548a2b59dc7275108563449b
miroslav <miroslav-dev@yandex.ru>
2025-10-08 22:09:23 +0200

    feat(api): endpoint for post delete

--- app/posts/router.py
+++ app/posts/router.py
@@ -15,7 +15,7 @@ async def add_post(post: Annotated[SPost, Depends()]):
     new_post = await PostsRepo.add_post(post)
     return new_post
 
-@router.get('/get_post', response_model = SResponsePost)
+@router.get('/get', response_model = SResponsePost)
 async def get_post_by_id(post_id: int):
     current_post = await PostsRepo.get_post(post_id)
     return current_post
@@ -24,3 +24,8 @@ async def get_post_by_id(post_id: int):
 async def get_posts():
     posts = await PostsRepo.get_all_posts()
     return posts
+
+@router.delete('/delete')
+async def delete_post(post_id: int):
+    deleted_post = await PostsRepo.delete_post(post_id)
+    return {'status':'success'}
