38e7cdc6f4dfaa5fecc2ac517bec8593aaec5746
miroslav <miroslav-dev@yandex.ru>
2025-10-09 21:09:43 +0200

    feat(api): include router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.posts.router import router as posts_router
+from app.comments.router import router as comments_router
 
 
 app = FastAPI()
 app.include_router(posts_router)
+app.include_router(comments_router)
 
