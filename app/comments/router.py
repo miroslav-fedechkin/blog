ccae4692b86c9af0611f9a673d4af95c3ecf537c
miroslav <miroslav-dev@yandex.ru>
2025-10-09 21:12:03 +0200

    feat(api): endpoint for add comments

--- app/comments/router.py
+++ app/comments/router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.comments.schemas import SComment
+from typing import Annotated
+from app.comments.repository import *
 
 router = APIRouter(
     prefix='/comments',
@@ -6,3 +9,7 @@ router = APIRouter(
 )
 
 
+@router.post('/add')
+async def add_comment_to_post(comment: Annotated[SComment, Depends()], post_id: int):
+    new_comment = await CommentsRepo.add_comment(comment, post_id)
+    return new_comment
