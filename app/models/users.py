ec97c16f385b537611a5169f2de12a523a08c3a1
Miroslav <fedechkin.miroslav@yandex.ru>
2026-03-12 21:54:27 +0200

    feat(models): initial users model

---
+++ app/models/users.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Integer
+
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
